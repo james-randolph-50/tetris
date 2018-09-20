dd22abbbb6289c6d10a03aae6ed3f676106f37fc
James <james.randolph2016@gmail.com>
2018-09-20 17:52:16 -0400

    iterating for shape placement

--- src/lib/index.js
+++ src/lib/index.js
@@ -4,5 +4,9 @@ function occupied(grid, x, y) {
 
 export function getActualCoordinates(newTetromino) {
     const coordinates = [],
-    
+    const { shape, offsetX, offsetY } = newTetromino;
+    const { blockUnit } = gameConstants;
+    for (let i = 0; i < shape.length; i++) {
+        for (let j = 0; j < shape.length; j++)
+    }
 }
