c87a65e2ac1e36ceba50680400974f1da0ef07ad
James <james.randolph2016@gmail.com>
2018-09-20 19:02:14 -0400

    function for the active tetromino. Also, I've been calling everyhting methods up until this point which was incorrect

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,3 +2,26 @@ import { combineReducers } from 'redux';
 import { getNewClearedGrid } from '../lib/index.js';
 import gameConstants from '../gameConstants.js';
 import * as actions from '../actions/index.js';
+
+const { initialGrid, tetrominos, blockUnit } = gameConstants;
+
+function gameStatus(state = 'IDLE', action) {
+    switch (action.type) {
+        case actions.START_GAME:
+        case actions.UNPAUSE_GAME:
+            return 'PLAYING';
+        case actions.PAUSE_GAME:
+            return 'PAUSED';
+        case actions.GAME_OVER:
+            return 'GAME_OVER';
+        default:
+            return state;
+    }
+}
+
+function activeTetrominos(state = initialGrid, action) {
+    switch (action.type) {
+        case actions.ADD_TETROMINO:
+            return getNewClearedGrid(state, action.currentTetromino, action.color);
+    }
+}
