a14d576de08bf7e07c65cdb7f90a4467a4b9eac8
James <james.randolph2016@gmail.com>
2018-09-20 19:34:52 -0400

    exporting consts

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,17 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from '../App';
-// import registerServiceWorker from '../registerServiceWorker';
+import gameConstants from '../gameConstants.js';
+import { rotateArray, checkCollisions, getCompletedLines } from '../lib/index.js';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
-
+export const SPAWN_TETROMINO = 'SPAWN_TETROMINO';
+export const ROTATE_TETROMINO = 'ROTATE_TETROMINO';
+export const START_GAME = 'START_GAME';
+export const STOP_GAME = 'STOP_GAME';
+export const GAME_OVER = 'GAME_OVER';
+export const CLEAR_LINE = 'CLEAR_LINE';
+export const ADD_SCORE = 'ADD_SCORE';
+export const MOVE_TETROMINO = 'MOVE_TETROMINO';
+export const MOVE_RIGHT = 'MOVE_RIGHT';
+export const MOVE_LEFT = 'MOVE_LEFT';
+export const MOVE_DOWN = 'MOVE_DOWN';
+export const ADD_TETROMINO = 'ADD_TETROMINO';
+export const PAUSE_GAME = 'PAUSE_GAME';
+export const UNPAUSE_GAME = 'UNPAUSE_GAME';
