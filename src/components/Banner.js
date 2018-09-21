26d85d0e6d50b850f5aed7f825ead0da167d45db
James <james.randolph2016@gmail.com>
2018-09-21 19:35:32 -0400

    set banner constant

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,3 +4,13 @@ import RaisedButton from 'material-ui/RaisedButton';
 import style from '../styles/styles.css'
 import { startGame } from '../actions/index.js';
 import { connect } from 'react-redux';
+
+let Banner = ({label, color, opacity, dispatch}) => {
+    const banner = {
+        background: `rgba(0,0,0,${opacity})`,
+        width: '100%',
+        height: '100%',
+        position: 'absolute',
+        top: '0px',
+    };
+}
