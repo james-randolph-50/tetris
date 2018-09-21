10ad0fae9785360b9f0de8fe37852b2154551e45
James <james.randolph2016@gmail.com>
2018-09-21 19:33:00 -0400

    importing files and style types

---
+++ src/components/Banner.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+import style from '../styles/styles.css'
+import { startGame } from '../actions/index.js';
+import { connect } from 'react-redux';
