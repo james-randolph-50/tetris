e221f47a00abbdebe7a37fa62df0ba6a75dfb987
James <james.randolph2016@gmail.com>
2018-09-21 20:00:08 -0400

    game info

--- src/components/GameInfo.js
+++ src/components/GameInfo.js
@@ -6,3 +6,15 @@ import style from '../styles/styles.css';
 import NextTetromino from '../containers/NextTetromino.js';
 import { changePauseState } from '../actions/index.js';
 
+let GameInfo =({points, clearedLines, nextTetromino, isPLaying, isPaused, isGameOver, dispatch}) => {
+    const buttonStyle = {
+        margin: '20% 0',
+    };
+    if (isPlaying) {
+        return (
+            <div className={style.gameInfo}>
+                <RaisedButton
+            </div>
+        )
+    }
+}
