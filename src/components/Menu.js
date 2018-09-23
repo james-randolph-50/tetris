19daf08bbb637ee6fe019bfc50d928a62364b0ce
James <james.randolph2016@gmail.com>
2018-09-23 14:27:55 -0400

    render

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,3 +1,18 @@
 import React from 'react';
 import style from '../styles/styles.css';
 import { loadMenu } from '../actions/index.js';
+
+class Menu extends React.Component {
+    componentDidMount() {
+        this.props.dispatch(loadMenu());
+    }
+    render() {
+        return (
+            <div>
+                <h1
+            </div>
+        )
+    }
+}
+
+export default Menu;
