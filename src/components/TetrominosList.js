d3d8d02fb977464717c9922cd5d9a592343fcf1f
James <james.randolph2016@gmail.com>
2018-09-24 18:57:12 -0400

    iterating thorugh grid and values

--- src/components/TetrominosList.js
+++ src/components/TetrominosList.js
@@ -1,3 +1,12 @@
 import React from 'react';
 import { Rect, Group } from 'react-konva';
 import gameConstants from '../gameConstants.js';
+
+const { blockUnit } = gameConstants;
+
+const TetrominosList = ({ grid }) => {
+    const arr = [];
+    grid.forEach((val, i) => {
+        val.forEach((block, j) => {})
+    })
+}
