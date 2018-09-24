e062117349dda8c0f16de9115928f847e496d666
James <james.randolph2016@gmail.com>
2018-09-24 18:37:43 -0400

    get coordinates function

--- src/components/Tetromino.js
+++ src/components/Tetromino.js
@@ -3,3 +3,8 @@ import { Rect, Group } from 'react-konva';
 import gameConstants from '../gameConstants.js';
 
 const { blockUnit } = gameConstants;
+
+function getCoordinates(shape) {
+    const coordinates = [];
+    for (let i = 0; i < shape.length; i++)
+}
