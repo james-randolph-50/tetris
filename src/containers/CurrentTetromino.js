ea1bf81782c45044569670ac52fc6ca38a2f5df7
James <james.randolph2016@gmail.com>
2018-09-24 19:11:01 -0400

    passing params to current tertomino

--- src/containers/CurrentTetromino.js
+++ src/containers/CurrentTetromino.js
@@ -1,4 +1,11 @@
 import { connect } from 'react-redux';
 import Tetromino from '../components/Tetromino.js';
 
-const mapStateToProps = ({ currentTetromino })
+const mapStateToProps = ({ currentTetromino }) => ({
+    shape: currentTetromino.shape,
+    color: currentTetromino.color,
+    name: currentTetromino.name,
+    offsetX: currentTetromino.offsetX,
+    offsetY: currentTetromino.offsetY,
+});
+
