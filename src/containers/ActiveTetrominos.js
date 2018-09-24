c6bb8cacd7a7f9cc783059730f7c9ad0f1ff6cd8
James <james.randolph2016@gmail.com>
2018-09-24 19:03:40 -0400

    active tetrominos there

--- src/containers/ActiveTetrominos.js
+++ src/containers/ActiveTetrominos.js
@@ -4,3 +4,9 @@ import TetrominosList from '../components/TetrominosList.js';
 const mapStateToProps = (state) => ({
     grid: state.activeTetrominos
 });
+
+const ActiveTetrominos = connect(
+    mapStateToProps
+)(TetrominosList);
+
+export default AciveTetrominos;
