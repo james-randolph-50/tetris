2a4f6af777427ae47cebb2da63026664be7c3178
James <james.randolph2016@gmail.com>
2018-09-24 18:59:00 -0400

    updates the tetromino in relation to gamefield

--- src/components/TetrominosList.js
+++ src/components/TetrominosList.js
@@ -5,8 +5,16 @@ import gameConstants from '../gameConstants.js';
 const { blockUnit } = gameConstants;
 
 const TetrominosList = ({ grid }) => {
-    const arr = [];
-    grid.forEach((val, i) => {
-        val.forEach((block, j) => {})
-    })
-}
+	const arr = [];
+	grid.forEach((val, i) => {
+		val.forEach((block, j) => {
+			if (block !== 'grey') {
+				const key = JSON.stringify({ x: i, y: j });
+				arr.push(<Rect key={key} width={blockUnit} height={blockUnit} x={i * 30} y={j * 30} fill={block} stroke="black" strokeWidth={8} />);
+			}
+		});
+	});
+	return <Group>{ arr }</Group>;
+};
+
+export default TetrominosList;
